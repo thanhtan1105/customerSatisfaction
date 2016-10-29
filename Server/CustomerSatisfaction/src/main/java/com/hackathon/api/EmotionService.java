d3cb8d2077e1c8d3dde991a867d8cdd4e70d4e56
trungnn1728 <trungnn1728@gmail.com>
2016-10-29 14:28:41 +0700

    incomplete back-end report emotion

--- Server/CustomerSatisfaction/src/main/java/com/hackathon/api/EmotionService.java
+++ Server/CustomerSatisfaction/src/main/java/com/hackathon/api/EmotionService.java
@@ -1,7 +1,13 @@
 package com.hackathon.api;
 
+import com.hackathon.constant.I_URI;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by HienTQSE60896 on 10/29/2016.
  */
+@RestController
+@RequestMapping(I_URI.API_EMOTION)
 public class EmotionService {
 }
---
+++ Server/CustomerSatisfaction/src/main/java/com/hackathon/constant/I_URI.java
@@ -0,0 +1,12 @@
+package com.hackathon.constant;
+
+/**
+ * Created by TrungNN on 10/29/2016.
+ */
+public interface I_URI {
+
+    /**
+     * Emotion
+     **/
+    String API_EMOTION = "/api/emotion";
+}
