65196d0a3f520886c55f61107ff982a250c566ae
trungnn1728 <trungnn1728@gmail.com>
2016-10-29 12:58:16 +0700

    customer emotion, authentication

--- Server/CustomerSatisfaction/src/main/java/com/hackathon/controller/EmotionControllerWeb.java
+++ Server/CustomerSatisfaction/src/main/java/com/hackathon/controller/EmotionControllerWeb.java
@@ -1,23 +1,30 @@
 package com.hackathon.controller;
 
 import com.hackathon.constant.IViewConst;
-import com.hackathon.constant.I_URI;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
 
 /**
  * Created by HienTQSE60896 on 10/29/2016.
  */
 @Controller
-@RequestMapping(I_URI.WEB_EMPLOYEE_EMOTION)
+@RequestMapping
 public class EmotionControllerWeb {
 
     private Logger logger = Logger.getLogger(EmotionControllerWeb.class);
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String loadCustomerEmotionView() {
+    @RequestMapping(value = "/emotion", method = RequestMethod.GET)
+    public String loadCustomerEmotionView(Model model) {
+        // get current date
+        Date currentDate = new Date();
+        //current date
+        model.addAttribute("CurrentDate", currentDate);
+
         return IViewConst.CUSTOMER_EMOTION_VIEW;
     }
 }
