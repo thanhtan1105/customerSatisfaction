a9ed8f388aeaad97eadc8f88cbb18e30a733b7d9
trungnn1728 <trungnn1728@gmail.com>
2016-10-29 11:23:24 +0700

    back-end customer emotion

---
+++ Server/CustomerSatisfaction/src/main/java/com/hackathon/constant/I_URI.java
@@ -0,0 +1,12 @@
+package com.hackathon.constant;
+
+/**
+ * Created by TrungNN on 10/29/2016.
+ */
+public interface I_URI {
+
+    /**
+     * WEB
+     */
+    String WEB_EMPLOYEE_EMOTION = "/employee/customer_emotion";
+}
--- Server/CustomerSatisfaction/src/main/java/com/hackathon/controller/EmotionControllerWeb.java
+++ Server/CustomerSatisfaction/src/main/java/com/hackathon/controller/EmotionControllerWeb.java
@@ -1,7 +1,23 @@
 package com.hackathon.controller;
 
+import com.hackathon.constant.IViewConst;
+import com.hackathon.constant.I_URI;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by HienTQSE60896 on 10/29/2016.
  */
+@Controller
+@RequestMapping(I_URI.WEB_EMPLOYEE_EMOTION)
 public class EmotionControllerWeb {
+
+    private Logger logger = Logger.getLogger(EmotionControllerWeb.class);
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String loadCustomerEmotionView() {
+        return IViewConst.CUSTOMER_EMOTION_VIEW;
+    }
 }
