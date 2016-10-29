5f163b59b6b4608efa36f31bb7ac7064e60ef3f7
TrungNN <trungnn1728@gmail.com>
2016-10-29 09:27:37 +0700

    commit server

---
+++ Server/CustomerSatisfaction/src/main/java/com/hackathon/CustomerSatisfactionApplication.java
@@ -0,0 +1,12 @@
+package com.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSatisfactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerSatisfactionApplication.class, args);
+	}
+}
